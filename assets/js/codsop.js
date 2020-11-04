c24ff4d1e791c2adc325049e880d52d853d38b17
kevintrejoutch <a1117320022@utch.edu.mx>
2020-11-03 20:28:12 -0700

    sopa-terminada

--- assets/js/codsop.js
+++ assets/js/codsop.js
@@ -2,4 +2,81 @@ $(document).ready(function(){
 	$(".head").hover(function(){
 		$(".head").addClass('resaltar');
 	});
+
+	$(".head").mouseleave(function(){
+		$(".head").removeClass('resaltar');
+	});
+
+	$(".purple").hover(function(){
+		$(".purple").addClass('resaltar');
+	});
+
+	$(".purple").mouseleave(function(){
+		$(".purple").removeClass('resaltar');
+	});
+
+	$(".black").hover(function(){
+		$(".black").addClass('resaltar');
+	});
+
+	$(".black").mouseleave(function(){
+		$(".black").removeClass('resaltar');
+	});
+
+	$(".pink").hover(function(){
+		$(".pink").addClass('resaltar');
+	});
+
+	$(".pink").mouseleave(function(){
+		$(".pink").removeClass('resaltar');
+	});
+
+	$(".arm").hover(function(){
+		$(".arm").addClass('resaltar');
+	});
+
+	$(".arm").mouseleave(function(){
+		$(".arm").removeClass('resaltar');
+	});
+
+	$(".orange").hover(function(){
+		$(".orange").addClass('resaltar');
+	});
+
+	$(".orange").mouseleave(function(){
+		$(".orange").removeClass('resaltar');	
+	});
+
+	$(".toes").hover(function(){
+		$(".toes").addClass('resaltar');
+	});
+
+	$(".toes").mouseleave(function(){
+		$(".toes").removeClass('resaltar');
+	});
+
+	$(".wh").hover(function(){
+		$(".wh").addClass('resaltar');
+	});
+
+	$(".wh").mouseleave(function(){
+		$(".wh").removeClass('resaltar');
+	});
+
+	$(".blue").hover(function(){
+		$(".blue").addClass('resaltar');
+	});
+
+	$(".blue").mouseleave(function(){
+		$(".blue").removeClass('resaltar');
+	});
+
+	$(".sho").hover(function(){
+		$(".sho").addClass('resaltar');
+	});
+
+	$(".sho").mouseleave(function(){
+		$(".sho").removeClass('resaltar');
+	});
+
 });
